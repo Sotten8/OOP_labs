566d5198d1f0403930f014f25a8076c0c6a3a609
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2025-10-04 11:50:05 +0300

    fix: make some changes to lab2 - add `Main` class - make changes to `shape_objects_editor.py` due to Main class

--- lab2/main.py
+++ lab2/main.py
@@ -2,77 +2,131 @@ import tkinter as tk
 from tkinter import messagebox
 from shape_objects_editor import ShapeObjectsEditor
 
-main = tk.Tk()
-main.geometry("600x400+400+150")
-main.title("Lab2")
-main.resizable(False, False)
+class Main:
+    def __init__(self):
+        self.main_window = None
+        self.canvas = None
+        self.editor_manager = None
+        self.current_mode = None
+        self.menu_bar = None
+        
+    def run(self):
+        self._create_window()
+        self._create_canvas()
+        self._create_editor()
+        self._create_menu()
+        self._bind_events()
+        self._set_default_editor()
+        self.main_window.mainloop()
+    
+    def _create_window(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("600x400+400+150")
+        self.main_window.title("Lab2")
+        self.main_window.resizable(False, False)
+    
+    def _create_canvas(self):
+        self.canvas = tk.Canvas(self.main_window, width=600, height=400, bg="white")
+        self.canvas.pack()
+    
+    def _create_editor(self):
+        self.editor_manager = ShapeObjectsEditor(self.canvas, self.main_window)
+    
+    def _create_menu(self):
+        self.current_mode = tk.StringVar(value="Point")
+        self.menu_bar = tk.Menu(self.main_window)
+        
+        self._create_file_menu()
+        self._create_objects_menu()
+        self._create_help_menu()
+        
+        self.main_window.config(menu=self.menu_bar)
+    
+    def _create_file_menu(self):
+        file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        file_menu.add_command(label="Exit", command=self.main_window.quit)
+        self.menu_bar.add_cascade(label="File", menu=file_menu)
+    
+    def _create_objects_menu(self):
+        objects_menu = tk.Menu(self.menu_bar, tearoff=0)
+        
+        objects_menu.add_radiobutton(
+            label="Крапка", 
+            command=self._set_point_editor, 
+            variable=self.current_mode, 
+            value="Point"
+        )
+        objects_menu.add_radiobutton(
+            label="Лінія", 
+            command=self._set_line_editor, 
+            variable=self.current_mode, 
+            value="Line"
+        )
+        objects_menu.add_radiobutton(
+            label="Прямокутник", 
+            command=self._set_rect_editor, 
+            variable=self.current_mode, 
+            value="Rectangle"
+        )
+        objects_menu.add_radiobutton(
+            label="Еліпс", 
+            command=self._set_ellipse_editor, 
+            variable=self.current_mode, 
+            value="Ellipse"
+        )
+        
+        self.menu_bar.add_cascade(label="Objects", menu=objects_menu)
+    
+    def _create_help_menu(self):
+        help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        help_menu.add_command(label="About", command=self._show_lab_information)
+        self.menu_bar.add_cascade(label="Help", menu=help_menu)
+    
+    def _set_point_editor(self):
+        self.editor_manager.start_point_editor()
+        self.current_mode.set("Point")
+        self.editor_manager.on_menu_popup()
+        self.main_window.title("Lab2 - Point Mode")
+    
+    def _set_line_editor(self):
+        self.editor_manager.start_line_editor()
+        self.current_mode.set("Line")
+        self.editor_manager.on_menu_popup()
+        self.main_window.title("Lab2 - Line Mode")
+    
+    def _set_rect_editor(self):
+        self.editor_manager.start_rect_editor()
+        self.current_mode.set("Rectangle")
+        self.editor_manager.on_menu_popup()
+        self.main_window.title("Lab2 - Rectangle Mode")
+    
+    def _set_ellipse_editor(self):
+        self.editor_manager.start_ellipse_editor()
+        self.current_mode.set("Ellipse")
+        self.editor_manager.on_menu_popup()
+        self.main_window.title("Lab2 - Ellipse Mode")
+    
+    def _show_lab_information(self):
+        messagebox.showinfo("Info", "Lab 2 is working fine\n(c) Copyright 2025")
+    
+    def _bind_events(self):
+        self.canvas.bind("<Button-1>", self._on_click)
+        self.canvas.bind("<B1-Motion>", self._on_drag)
+        self.canvas.bind("<ButtonRelease-1>", self._on_drop)
+    
+    def _on_click(self, event):
+        self.editor_manager.on_click(event.x, event.y)
+    
+    def _on_drag(self, event):
+        self.editor_manager.on_drag(event.x, event.y)
+    
+    def _on_drop(self, event):
+        self.editor_manager.on_drop(event.x, event.y)
+    
+    def _set_default_editor(self):
+        self._set_point_editor()
 
-canvas = tk.Canvas(main, width=600, height=400, bg="white")
-canvas.pack()
 
-editor_manager = ShapeObjectsEditor(canvas)
-
-def on_click(event):
-    editor_manager.on_click(event.x, event.y)
-
-def on_drag(event):
-    editor_manager.on_drag(event.x, event.y)
-
-def on_drop(event):
-    editor_manager.on_drop(event.x, event.y)
-
-canvas.bind("<Button-1>", on_click)
-canvas.bind("<B1-Motion>", on_drag)
-canvas.bind("<ButtonRelease-1>", on_drop)
-
-def show_lab_information():
-    messagebox.showinfo("Info", "Lab 2 is working fine\n(c) Copyright 2025")
-
-current_mode = tk.StringVar(value="Point")
-
-def set_point_editor():
-    editor_manager.start_point_editor()
-    current_mode.set("Point")
-    editor_manager.on_menu_popup()
-    main.title("Lab2 - Point Mode")
-
-def set_line_editor():
-    editor_manager.start_line_editor()
-    current_mode.set("Line")
-    editor_manager.on_menu_popup()
-    main.title("Lab2 - Line Mode")
-
-def set_rect_editor():
-    editor_manager.start_rect_editor()
-    current_mode.set("Rectangle")
-    editor_manager.on_menu_popup()
-    main.title("Lab2 - Rectangle Mode")
-
-def set_ellipse_editor():
-    editor_manager.start_ellipse_editor()
-    current_mode.set("Ellipse")
-    editor_manager.on_menu_popup()
-    main.title("Lab2 - Ellipse Mode")
-
-menu_bar = tk.Menu(main)
-
-file_menu = tk.Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Exit", command=main.quit)
-menu_bar.add_cascade(label="File", menu=file_menu)
-
-objects_menu = tk.Menu(menu_bar, tearoff=0)
-objects_menu.add_radiobutton(label="Крапка", command=set_point_editor, variable=current_mode, value="Point")
-objects_menu.add_radiobutton(label="Лінія", command=set_line_editor, variable=current_mode, value="Line")
-objects_menu.add_radiobutton(label="Прямокутник", command=set_rect_editor, variable=current_mode, value="Rectangle")
-objects_menu.add_radiobutton(label="Еліпс", command=set_ellipse_editor, variable=current_mode, value="Ellipse")
-menu_bar.add_cascade(label="Objects", menu=objects_menu)
-
-help_menu = tk.Menu(menu_bar, tearoff=0)
-help_menu.add_command(label="About", command=show_lab_information)
-menu_bar.add_cascade(label="Help", menu=help_menu)
-
-main.config(menu=menu_bar)
-
-set_point_editor()
-
-main.mainloop()
+if __name__ == "__main__":
+    app = Main()
+    app.run()
--- lab2/shape_objects_editor.py
+++ lab2/shape_objects_editor.py
@@ -1,8 +1,9 @@
 from editor import PointEditor, LineEditor, RectEditor, EllipseEditor
 
 class ShapeObjectsEditor:
-    def __init__(self, canvas):
+    def __init__(self, canvas, main_window=None):
         self._canvas = canvas
+        self._main = main_window
         self.__shapes = [None] * 122
         self.__shapes_count = 0
         self.__current_editor = None
@@ -37,9 +38,11 @@ class ShapeObjectsEditor:
             self.__current_editor.on_drag(x, y)
     
     def _redraw(self):
+        self._canvas.delete("all")
         for i in range(self.__shapes_count):
             if self.__shapes[i]:
                 self.__shapes[i].show(self._canvas)
     
     def on_menu_popup(self):
-        self.__current_editor.on_menu_popup()
+        if self.__current_editor:
+            self.__current_editor.on_menu_popup()
